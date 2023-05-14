a6335647ef099a7d8559d48d5a46f8d627b88630
pavelpopovich1993 <pavelpopovich932@gmail.com>
2023-05-15 00:13:00 +0300

    create dialod accordion header

--- js/scripts.js
+++ js/scripts.js
@@ -16,3 +16,28 @@ $(document).ready(function(){
         }
     });
 });
+
+$( function() {
+    $( "#accordion" ).accordion({
+        collapsible: true
+    });
+} );
+
+
+$( function() {
+    $( "#dialog" ).dialog({
+        autoOpen: false,
+        show: {
+            effect: "blind",
+            duration: 1000
+        },
+        hide: {
+            effect: "explode",
+            duration: 1000
+        }
+    });
+
+    $( "#opener" ).on( "click", function() {
+        $( "#dialog" ).dialog( "open" );
+    });
+} );
